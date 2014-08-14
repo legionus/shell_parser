33f2bc785c159a23b47d251885ffd3daae368e76
Oleg Bulatov <oleg@bulatov.me>
2014-08-15 00:06:34 +0400

    Add print to FuncDef
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- ShellParser/Token/FuncDef.pm
+++ ShellParser/Token/FuncDef.pm
@@ -14,4 +14,17 @@ sub new {
     }, $class);
 }
 
+sub print {
+    my ($self, $sep, $depth) = @_;
+    print $sep x $depth . "FuncDef(name=$self->{name})\n";
+    print $sep x $depth . $sep . "FuncDef::body()\n";
+    $self->{body}->print($sep, $depth + 2);
+    if ($self->{redirect}) {
+    	print $sep x $depth . $sep . "FuncDef::redirect()\n";
+    	foreach my $redirect (@{$self->{redirect}}) {
+		    $redirect->print($sep, $depth + 2);
+    	}
+    }
+}
+
 1;
