776da0301112ecaaba377d3728cad99158ff5855
Alexey Gladkov <gladkov.alexey@gmail.com>
2014-08-14 17:43:46 +0400

    Add Token::Braces
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

---
+++ Token/Braces.pm
@@ -0,0 +1,15 @@
+package ShellParser::Token::Braces;
+
+use strict;
+use warnings;
+
+use base 'ShellParser::Token';
+
+sub new {
+    my ($class, $body) = @_;
+    return bless({
+        body => $body,
+    }, $class);
+}
+
+1;
