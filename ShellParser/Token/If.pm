8fa5c69754139fe2f56aaa502ea9d745c0334fd4
Oleg Bulatov <oleg@bulatov.me>
2014-08-16 20:06:36 +0400

    Removed tabs
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- ShellParser/Token/If.pm
+++ ShellParser/Token/If.pm
@@ -8,28 +8,28 @@ use base 'ShellParser::Token';
 sub new {
     my ($class, $condition, $body) = @_;
     return bless({
-    	body => [
-    		{condition => $condition, body => $body},
-    	],
+        body => [
+            {condition => $condition, body => $body},
+        ],
     }, $class);
 }
 
 sub append {
-	my ($self, $if_block) = @_;
-	push @{$self->{body}}, @{$if_block->{body}};
+    my ($self, $if_block) = @_;
+    push @{$self->{body}}, @{$if_block->{body}};
 }
 
 sub print {
     my ($self, $sep, $depth) = @_;
     print $sep x $depth . "If()\n";
     foreach my $elem (@{$self->{body}}) {
-    	if (defined($elem->{condition})) {
-		    print $sep x $depth . $sep . "If::condition()\n";
-        	$elem->{condition}->print($sep, $depth + 2);
-    	} else {
-		    print $sep x $depth . $sep . "If::else()\n";
-    	}
-		print $sep x $depth . $sep . "If::then()\n";
+        if (defined($elem->{condition})) {
+            print $sep x $depth . $sep . "If::condition()\n";
+            $elem->{condition}->print($sep, $depth + 2);
+        } else {
+            print $sep x $depth . $sep . "If::else()\n";
+        }
+        print $sep x $depth . $sep . "If::then()\n";
         $elem->{body}->print($sep, $depth + 2);
     }
 }
