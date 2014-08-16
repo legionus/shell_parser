afc3f6124d76f34475e20e4c8d41cf32ba5db41d
Oleg Bulatov <oleg@bulatov.me>
2014-08-16 22:36:59 +0400

    Exit with non-zero code on error
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- test_parser.pl
+++ test_parser.pl
@@ -10,9 +10,11 @@ use ShellParser;
 open(my $fh, '<', $ARGV[0]) or die $!;
 my $p = ShellParser->new();
 my $result = $p->parse($fh);
+close($fh);
+
 if (!$result) {
     print $p->error;
+    exit(1);
 } else {
     $result->print(".   ", 0);
 }
-close($fh);
