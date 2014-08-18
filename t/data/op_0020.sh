084c9b9f985d8386e91a42dbceb2da98766a0549
Oleg Bulatov <dmage@yandex-team.ru>
2014-08-18 13:55:52 +0400

    Update HEREDOC examples
    Signed-off-by: Oleg Bulatov <dmage@yandex-team.ru>

--- t/data/op_0020.sh
+++ t/data/op_0020.sh
@@ -7,12 +7,17 @@ $foo
 cat <<"E'O'F"
 line3
 E'O'F
+cat <<foo\"bar\"baz
+line4
+foo"bar"baz
+
+# begin bashism
 cat <<$(echo "foo")
-line4 $(pwd)
-$(echo foo)
-cat <<$(echo foo)
 line5 $(pwd)
 $(echo foo)
-cat <<$(echo foo)""
+cat <<$(echo foo)
 line6 $(pwd)
+$(echo foo)
+cat <<$(echo foo)""
+line7 $(pwd)
 $(echo foo)
